1af643762051991250ed2731426d94aaa877ac6a
Renata Cabral <renata.kc@gmail.com>
2018-05-04 00:01:14 +0100

    deleted one block, left one one get

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send({ express: 'Hello From Express\n', datetime });
+  res.send({ express: 'Hello From Express', datetime });
 
 });
 
-app.get('/date', (req, res) => {
-  res.send({ express: datetime });
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
