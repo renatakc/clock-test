9a4220e5500a4e8338c517d9c1ff78e9f08b8aa8
Renata Cabral <renata.kc@gmail.com>
2018-05-03 23:16:53 +0100

    requested by Diego - added var datetime to get current date and time

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var datetime = new Date();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+  res.send({ express: 'Hello From Express\n', datetime });
+
+});
+
+app.get('/date', (req, res) => {
+  res.send({ express: datetime });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
