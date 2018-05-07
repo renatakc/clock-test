12acb9698d33c0e23cad8297cfbefd77c28ea127
Renata Cabral <renata.kc@gmail.com>
2018-05-07 19:50:01 +0100

    added var datetime to app.get
    to refresh datetime every time the app starts running

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var datetime = new Date();
+//var datetime = new Date();
+console.log("Created datetime")
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send({ express: 'Hello From Express', datetime });
 
+app.get('/', (req, res) => {
+  var datetime = new Date();
+  res.send({ express: 'Hello From Express', datetime});
 });
 
 
